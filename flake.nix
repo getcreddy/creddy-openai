83723d0627ed4827d815f5ec73c9d433bdc5bebc
Rooty <rooty@creddy.dev>
2026-02-23 02:53:02 +0000

    feat: add nix flake and direnv support

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "creddy-openai - Plugin for Creddy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_24
+            gopls
+            oras
+          ];
+
+          shellHook = ''
+            export GOPATH="$HOME/go"
+            export PATH="$GOPATH/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
